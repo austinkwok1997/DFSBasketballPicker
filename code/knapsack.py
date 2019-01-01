116779a618d2a7721ca3e32d41e7a1908ee35e39
Austin Kwok <austin.kwok@protonmail.com>
2019-01-01 13:12:51 -0800

    Added Knapsack class

---
+++ code/knapsack.py
@@ -0,0 +1,7 @@
+import numpy as np
+from numpy.linalg import solve
+from scipy.optimize import approx_fprime
+
+class KnapSack:
+    def KS(self,cost,points,salary):
+        return NotImplementedError()
